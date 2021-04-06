a5c0179e7c561a10c75df13e527a8383251c74a9
Jacó Rocha Fernandes <jacofernandes@outlook.com>
2021-04-06 10:36:42 -0300

    colocando valor fixo para a tabela fornecedores

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
+    //protected $table = 'nome da tabela no banco';
+    protected  $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
     use HasFactory;
 }
